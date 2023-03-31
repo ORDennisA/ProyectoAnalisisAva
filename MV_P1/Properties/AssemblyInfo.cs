28620d05308952c3251f946e848d8e9a8baaa9e9
Jose Luis <jose.nunez18@tectijuana.edu.mx>
2023-03-30 20:01:40 -0700

    Actualizacion con Ventas

---
+++ MV_P1/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MV_P1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MV_P1")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e742caf7-068d-46f6-922c-d6028ebb7815")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ MV_P1/Scripts/js_TipoLibros.js
@@ -0,0 +1,19 @@
+﻿function guardarTipoLibros() {
+
+    $.ajax({
+        type: "POST",
+        url: UrlGuardarTipoLibros,
+        async: true,
+        data: {
+            EstanteLibros: document.getElementById("Esta").value,
+            TematicaLibros: document.getElementById("Tema").value,
+
+        },
+        success: function (data) {
+            alert("Registro exitoso");
+        },
+        error: function (xhr, status, error) {
+            alert("error");
+        }
+    });
+}
