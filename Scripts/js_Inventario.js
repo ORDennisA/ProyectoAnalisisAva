59c5f0c1c2f01b148af0b2aa68e991128ddd19d2
ORDennisA <89487953+ORDennisA@users.noreply.github.com>
2023-05-07 22:40:22 -0700

    Add files via upload

---
+++ Scripts/js_Inventario.js
@@ -0,0 +1,19 @@
+﻿function guardarInventario() {
+
+    $.ajax({
+        type: "POST",
+        url: UrlInventario,
+        async: true,
+        data: {
+            Cantidad_de_Libro: document.getElementById("CantLib").value,
+            Fecha_Adquisicion: document.getElementById("FechAd").value,
+            id_Libro: document.getElementById("idLib").value
+        },
+        success: function (data) {
+            alert("Registro exitoso");
+        },
+        error: function (xhr, status, error) {
+            alert("error");
+        }
+    });
+}
