df36f97daefb1fd930bf22bfc712cdf9f97738e4
LizardoJoestar <khasdragon@tutanota.com>
2023-05-11 18:56:00 -0700

    Added report view toggle

--- Scripts/js_Reportes.js
+++ Scripts/js_Reportes.js
@@ -1,5 +1,15 @@
-﻿function getReporte() {
+﻿// Globals
 
+var toggle = "ventas";
+var tableVentas = document.getElementById("reporteVentas");
+var tablePrestamos = document.getElementById("reportePrestamos");
+var tipoReporte = document.getElementById("tipoReporte");
+
+
+// Functions
+
+function getReporte() {
+    // Sends form info to controller, gets a json data object, and then displays it
     $.ajax({
         type: "GET",
         url: UrlReportes,
@@ -24,3 +34,31 @@ function limpiarFormulario() {
     document.getElementById("fechaInicio").value = "";
     document.getElementById("fechaFinal").value = "";
 } 
+
+function toggleReport() {
+    // Both reports hidden first
+    tableVentas.style.display = "none";
+    tablePrestamos.style.display = "none";
+
+    // Show reports as selected by user
+    tipoReporte.addEventListener("change", () => {
+        toggle = document.getElementById("tipoReporte").value;         
+        console.log(toggle);
+
+        if (toggle == "ventas") {
+            tableVentas.style.display = "block";
+            tablePrestamos.style.display = "none";
+        }
+        else if (toggle == "prestamos") {
+            tableVentas.style.display = "none";
+            tablePrestamos.style.display = "block";
+        }
+        else {
+            tableVentas.style.display = "none";
+            tablePrestamos.style.display = "none";
+        }
+    });
+}
+
+// Initialize event listener(s)
+toggleReport();
